c3ba8d79faf72f0275894468af50173f7418f942
theronanlarkin <theronanlarkin@gmail.com>
2022-03-21 02:33:35 +0000

    Added back button functionality to main screen to prevent looping between home and results page

--- app/src/main/java/com/example/mbpersonalitytest/MainActivity.java
+++ app/src/main/java/com/example/mbpersonalitytest/MainActivity.java
@@ -25,4 +25,13 @@ public class MainActivity extends AppCompatActivity {
         Intent i = new Intent(this, History.class);
         startActivity(i);
     }
+
+    //makes sure back button exits app when pressed
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
 }
